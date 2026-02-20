7d686d1f2ded195084903345497c3d6e489cc9fd
David Show <david.show5c@gmail.com>
2026-02-20 07:27:46 -0600

    Initial Commit

---
+++ jokeGeneratorAppShowJack/jokeGeneratorAppShowJackApp.swift
@@ -0,0 +1,17 @@
+//
+//  jokeGeneratorAppShowJackApp.swift
+//  jokeGeneratorAppShowJack
+//
+//  Created by DAVID SHOW on 2/20/26.
+//
+
+import SwiftUI
+
+@main
+struct jokeGeneratorAppShowJackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
