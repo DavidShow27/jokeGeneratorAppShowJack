3aef679b7df946b06d043937f2399a25778abfb0
David Show <david.show5c@gmail.com>
2026-02-23 09:32:43 -0600

    Integrate Firebase into the app with AppDelegate

--- jokeGeneratorAppShowJack/jokeGeneratorAppShowJackApp.swift
+++ jokeGeneratorAppShowJack/jokeGeneratorAppShowJackApp.swift
@@ -6,14 +6,23 @@
 //
 
 import SwiftUI
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
-struct jokeGeneratorAppShowJackApp: App {
-    
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct YourApp: App {
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
-    
+  }
 }
