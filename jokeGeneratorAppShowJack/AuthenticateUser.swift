209dbdc3032e92145f18937bd7d96697d65d8947
David Show <david.show5c@gmail.com>
2026-02-23 09:25:46 -0600

    Implement user authentication with FirebaseAuth

--- jokeGeneratorAppShowJack/AuthenticateUser.swift
+++ jokeGeneratorAppShowJack/AuthenticateUser.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+class AuthViewModel: ObservableObject {
+    
+    @Published var user: User?
+    @Published var isAnonymous = false
+    
+    private var handle: AuthStateDidChangeListenerHandle?
+    
+    init() {
+        handle = Auth.auth().addStateDidChangeListener { _, user in
+            self.user = user
+            self.isAnonymous = user?.isAnonymous ?? false
+        }
+    }
+    
+    func signIn(email: String, password: String) async throws {
+        try await Auth.auth().signIn(withEmail: email, password: password)
+    }
+    
+    func signUp(email: String, password: String) async throws {
+        try await Auth.auth().createUser(withEmail: email, password: password)
+    }
+    
+    func continueAsGuest() async throws {
+        try await Auth.auth().signInAnonymously()
+    }
+    
+    func signOut() throws {
+        try Auth.auth().signOut()
+    }
+}
