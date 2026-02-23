937bf30515743ae9ab2ae91ac3c656893d82429d
jack <jackson.lisle@icloud.com>
2026-02-23 08:50:38 -0600

    tried to add api

--- jokeGeneratorAppShowJack/ContentView.swift
+++ jokeGeneratorAppShowJack/ContentView.swift
@@ -11,12 +11,50 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("QuickQuip")
+                .font(.largeTitle)
+            
+            Spacer()
+            
+            
         }
-        .padding()
+        .onAppear(){
+            getJoke()
+        }
+        
+    }
+    func getJoke(){
+        
+        let session = URLSession.shared
+        
+        //creating URL for api call (you need your apikey)
+        let weatherURL = URL(
+            string:
+                ""
+        )!
+        
+        // Making an api call and creating data in the completion handler
+        let dataTask = session.dataTask(with: weatherURL) {
+            // completion handler: happens on a different thread, could take time to get data
+            (data: Data?, response: URLResponse?, error: Error?) in
+            
+            if let error = error {
+                print("Error:\n\(error)")
+            } else {
+                // if there is data
+                if let data = data {
+                    // convert data to json Object
+                    if let jsonObj = try? JSONSerialization.jsonObject(
+                        with: data,
+                        options: .allowFragments
+                    ) as? NSDictionary {
+                        print(jsonObj)
+                    }
+                }
+            }
+        }
+    
+        dataTask.resume()
     }
     
 }
