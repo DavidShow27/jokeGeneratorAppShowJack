8ac82854161be3b47eaf6c6804ab006d88497172
David Show <david.show5c@gmail.com>
2026-02-23 09:29:40 -0600

    Modify HomeView to show logged in status
    Updated HomeView to display login status and sign out button.

--- jokeGeneratorAppShowJack/HomeView.swift
+++ jokeGeneratorAppShowJack/HomeView.swift
@@ -5,14 +5,24 @@
 //  Created by DAVID SHOW on 2/23/26.
 //
 
+
 import SwiftUI
 
 struct HomeView: View {
+    
+    @EnvironmentObject var auth: AuthViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Logged In")
+            
+            Button("Sign Out") {
+                try? auth.signOut()
+            }
+        }
     }
 }
-
 #Preview {
     HomeView()
+        .enviormentObject(AuthViewModel())
 }
